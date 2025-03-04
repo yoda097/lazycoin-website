6249b49527bf31f1cdadcffbe61066ecf76eabc6
yoda097 <elcin09852@gmail.com>
2025-03-04 13:48:32 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.animated').forEach(el => {
+        el.style.opacity = 1;
+    });
+    console.log("LazyCoin website loaded with animations!");
+});
