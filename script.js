ca90d5a9ba6a7488eab95e25e3704bce687892df
yoda097 <elcin09852@gmail.com>
2025-03-05 17:08:01 +0400

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.querySelectorAll('.animated').forEach(el => {
-        el.style.opacity = 1;
-    });
-    console.log("LazyCoin website loaded with animations!");
-});
