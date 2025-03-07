2df923cf4a679d684ca9bdb28ac365515f98046a
yoda097 <elcin09852@gmail.com>
2025-03-07 15:36:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,9 @@ document.addEventListener("DOMContentLoaded", function() {
         starContainer.appendChild(star);
     }
 });
+
+// 🌙 Toggle Theme Function
+document.getElementById("themeToggle").addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
+});
