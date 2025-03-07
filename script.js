fd0621d5721f90ba9a0d898cb4f37d898b2de076
yoda097 <elcin09852@gmail.com>
2025-03-07 13:27:10 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('language-select').addEventListener('change', function() {
     alert('Language selection will be implemented soon!');
 });
+
+function toggleMenu() {
+    var menu = document.querySelector(".menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
