759c1e0b350f1863d5fb352f12ece666599527de
yoda097 <elcin09852@gmail.com>
2025-03-07 13:28:54 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-document.getElementById('language-select').addEventListener('change', function() {
-    alert('Language selection will be implemented soon!');
+document.addEventListener("DOMContentLoaded", function() {
+    var languageSelect = document.getElementById('language-select');
+    if (languageSelect) {
+        languageSelect.addEventListener('change', function() {
+            alert('Language selection will be implemented soon!');
+        });
+    }
 });
 
 function toggleMenu() {
     var menu = document.querySelector(".menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
+    if (menu) {
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "flex"; // "block" yerine "flex" kullanarak hizalamayı bozma
+        }
     }
 }
